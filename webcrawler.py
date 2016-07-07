8a393d9074e49e138daff9ac56fe18c2b7217902
dagius9444 <dagius9444@gmail.com>
2016-07-07 15:23:15 +0100

    Fixed processing_page function logic.
    Also added code for creating absolute urls from relative urls.

--- webcrawler.py
+++ webcrawler.py
@@ -25,9 +25,38 @@ def extract_hyperlinks(content):
     return hyperlinks
 
 
+def test_absurl():
+    u = absurl("/about/", "http://site-of-interest")
+    assert u == "http://site-of-interest/about/"
+
+def absurl(rel_url, base_url):
+    """Return the absolute url."""
+    if rel_url == "/":
+        return base_url
+    return base_url + rel_url
+
+def test_filter_hyperlinks():
+    hyperlinks = ["https://www.google.co.uk/",
+                  "http://site-of-interest/training/"]
+    filtered = filter_hyperlinks(hyperlinks, "http://site-of-interest")
+    assert filtered == ["http://site-of-interest/training/"], filtered
+
+
+def absurl_filter(hyperlinks, base_url):
+    """Return list where relative urls have been converted to absolute urls."""
+    filtered = []
+    if base_url.endswith("/"):
+        base_url = base_url[:-1]
+    for hlink in hyperlinks:
+        if hlink.startswith("/"):
+            hlink = absurl(hlink, base_url)
+        filtered.append(hlink)
+    return filtered
+
 def filter_hyperlinks(hyperlinks, base_url):
     """Return list of hyperlinks belonging to base_url."""
     return [hlink for hlink in hyperlinks if hlink.startswith(base_url)]
+
 
 
 def fetch_content(url):
@@ -39,16 +68,19 @@ def fetch_content(url):
 
 
 def process_page(url, base_url):
-    print("Page:", url)
+    print("Processing page:", url)
     content = fetch_content(url)
     hyperlinks = extract_hyperlinks(content)
-    hyperlinks = filter_hyperlinks(hyperlinks, url)
+    hyperlinks = absurl_filter(hyperlinks, base_url)
+    hyperlinks = filter_hyperlinks(hyperlinks, base_url)
+    print("Hyperlinks in page:", hyperlinks)
     for hlink in hyperlinks:
         if not hlink in VISITED_PAGES:
             print(hlink)
             process_page(hlink, base_url)
         else:
-            print("other url:", hlink)
+            print("already processed:", hlink)
+
 
 def main():
     parser = argparse.ArgumentParser(description=__doc__)
